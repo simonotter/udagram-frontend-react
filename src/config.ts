cfdd831820a8267a12372bb65407722191a80ea2
Simon Otter <simonotter@gmail.com>
2020-11-26 18:33:45 +0000

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = 'https://4egkqawm32.execute-api.us-east-1.amazonaws.com/dev'
+
+export const authConfig = {
+  domain: 'dev--otter.eu.auth0.com',
+  clientId: 'sOvQo2rdS7o3g37NZ2QXuucCumGc32Sg',
+  callbackUrl: 'http://localhost:3000/callback'
+}
